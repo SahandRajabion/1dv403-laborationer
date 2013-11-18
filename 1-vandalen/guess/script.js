ba9be8104aeef165881ef1709a363334e7537ccb
OpenShift guest <sr222hn@student.lnu.se>
2013-11-18 08:08:07 -0500

    hemligt tal, snart klar

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,22 +2,49 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
-	
+	var secret =Math.floor( Math.random(secret) * 100)+1; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var input = 0; 
 	// I denna funktion ska du skriva koden för att hantera "spelet"
+	
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
 			
 		// Plats för förändring.
+       input++;
+           
+            // [false, "Talet är utanför intervallet 0 - 100"]	
+            if (number > 100 || number < 0)
+            alert("Talet är utanför intervallet 0 - 100");
+           
+            else{
+                
+            // [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
+            if (number === secret) 
+            alert("Grattis du vann! Det hemliga talet var" +(secret)+ "och du behövde" +(input)+ "gissningar för att hitta det."); 
+                
+                
+            // [false, "Det hemliga talet är högre!"]
+            if (number < secret)
+            alert("Det hemliga talet är högre!");
+            
+       
+            // [false, "Det hemliga talet är lägre!"]
+            if (number > secret)
+            alert("Det hemliga talet är lägre!");
+            
+            if (isNaN(number))
+            alert("Ange siffor!");
 
+            }
+              
+            };
+	
+        
 
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
-	};
+		
+	
+		
 	
 	// ------------------------------------------------------------------------------
 
