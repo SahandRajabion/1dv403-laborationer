9dbb7c08748a32e0dfc27233ec6f408405b0bb67
OpenShift guest <sr222hn@student.lnu.se>
2013-12-10 15:21:12 -0500

    skapat tabell funktionen samt refererat till html ID: et

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -16,9 +16,25 @@ init:function(){
 // Här anropas "getPictureArray" i filen "random.js" som slumpar bilderna och sparar resultatet i randomMemory arrayen ovan.
 Memory.randomMemory = RandomGenerator.getPictureArray(columns, rows);
 
-console.log(Memory.randomMemory);
+//Anropar "createTable" för att kunna visa tabellen.
+Memory.createTable();},
+
+//Funktion som ritar upp själva tabellen.
+createTable:function(){
     
-},
+    // För att skapa en tabell i funktionen createTable som sedan anropas av "Memory.CreateTable" som visar tabellen i fönstret.
+    var table = document.createElement("table");
+    
+    // Sätter in i "memoryBoard" i html filen.
+    document.getElementById("memoryBoard").appendChild(table);
+    
+    
+    
+    
+}
+
+
+
     
  };
  window.onload = Memory.init;
