c2ff9cb23ff433f887a8be77f929c3bd14d6172a
OpenShift guest <sr222hn@student.lnu.se>
2013-12-11 09:09:33 -0500

    Skapat själva tabellen som bilderna ska ligga i

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,5 +1,5 @@
 "use strict"
-var columns = 4;
+var cols = 4;
 var rows = 4;
 
 var Memory= {
@@ -14,7 +14,7 @@ randomMemory: [],
 init:function(){
     
 // Här anropas "getPictureArray" i filen "random.js" som slumpar bilderna och sparar resultatet i randomMemory arrayen ovan.
-Memory.randomMemory = RandomGenerator.getPictureArray(columns, rows);
+Memory.randomMemory = RandomGenerator.getPictureArray(cols, rows);
 
 //Anropar "createTable" för att kunna visa tabellen.
 Memory.createTable();},
@@ -28,7 +28,29 @@ createTable:function(){
     // Sätter in i "memoryBoard" i html filen.
     document.getElementById("memoryBoard").appendChild(table);
     
-    
+    // Genererar upp tabellen som bilderna kommer ligga i. Loopar med hjälp av en for sats, de antal rader som tabellen ska innehålla.
+    for (var row = 0; row< rows; row++){
+      
+      //Lägger in alla loopade "tr" taggar i variabeln table.
+        var tr = document.createElement("tr");
+        table.appendChild(tr);
+        
+        // Genererar upp antal kolumner i tabellen.
+        for (var col=0; col<cols; col++){
+            
+        // Lägger in alla loopade "td" taggar i de loopade "tr" taggar som redan skapats.
+        var td = document.createElement("td");
+        tr.appendChild(td);
+        
+        
+        
+        
+        
+        
+        
+        }
+        
+    }
     
     
 }
