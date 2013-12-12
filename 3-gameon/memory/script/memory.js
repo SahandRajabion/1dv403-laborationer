b5460de52c4a808b5bc9d24418133817dfd638b7
OpenShift guest <sr222hn@student.lnu.se>
2013-12-12 09:18:30 -0500

    senaste uppdatering ej klar

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -12,6 +12,11 @@ randomMemory: [],
 //Array som lagrar alla a-taggat.
 atags: [],
 
+//Antal rätta par
+countPair: 0,
+
+// Antal försök
+countTries: 0,
    
 init:function(){
    
@@ -68,36 +73,78 @@ createTable:function(){
     
 },
 
-turn: function(a,b){
+turn: function(q, b){
+    
+    console.log(q, b);
+    
+
+    
     b.onclick= function(){
       
-      // Ska vara startbilden "0.png" och ej ngt annat, annars retunera "False".
-        if(this.getElementByTagName("img")[0].getAttribute("src") !== "pics/o.png")
+      
+      // Ska vara startbilden "0.png" och ej ngt annat, annars retunera "False", så alltid frågetecknet visas innan mus-klick.
+        if(this.getElementsByTagName("img")[0].getAttribute("src") !== "pics/0.png")
         {
             return false;
         }
-    }
+    };
+    
+    console.log(b);
     
     // Sätter in "a" taggen i arrayen "atags"
     Memory.atags.push(1);
-   
-   // Om arrayens längs bara uppnår 1 eller 2 element , visas bilden upp.
+   // Om arrayens längd bara uppnår 1 eller 2 element , visas bilden upp, alltså vid varje klick (ska bara kunna klicka på startbilden för vändning.
    if (Memory.atags.length === 1 || Memory.atags.length === 2) {
-        this.getElementsByTagName("img")[0].setAttribute("src", "pics/" + Memory.randomArray[a] + ".png");
+        console.log(Memory.randomMemory[q]);
+        
+        this.getElementsByTagName("img")[0].setAttribute("src", "pics/" + Memory.randomMemory[q] + ".png");
     }
     
     //Skulle bilderna ej stämma, anropas funktionen "seeMems" för att vända tillbaka efter 1 sec.
     if (Memory.atags.length == 2)
-    { setTimout(function(){ Memory.seeMems(Memory.atags);
+    { setTimeout(function(){ Memory.seeMems(Memory.atags);
     }, 1000);}
 
  
+},
 
 
 
 
 
 
+// Testar om rutorna är likadana så kommer den visas annars vänds dem tillbaka. 
+seeMems: function(memo){
+if (memo[0].getElementsByTagName("img")[0].getAttribute("src") === memo[1].getElementsByTagName("img")[0].getAttribute("src"))
+{
+    
+// Räkna upp antal rätta par
+Memory.countPair++;
+
+// Är antal rätta par hälften av alla brickor på spelplanen har man vunnit spelet.
+if (Memory.countPair === ((cols*rows)/2)){
+alert("Grattis du har vunnit spelet!\nAntalförsök:" + Memory.countTries);
+}
+
+// Tömmer innehållet i arrayen
+Memory.atags = [];
+}
+
+else
+{
+// Startbilden visas igen, arrayens innehåll töms
+memo[0].getElementsByTagName("img")[0].setAttribute("src", "pics/0.png");
+memo[1].getElementsByTagName("img")[0].setAttribute("src", "pics/0.png");
+
+Memory.atags = [];
+
+// och räknas upp som ett försök
+Memory.countTries++;
+}
+
+
+}
+};
 
 
 
