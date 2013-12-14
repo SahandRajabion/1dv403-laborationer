2c1b7858364d4ad260d2bf349190894069dd824c
OpenShift guest <sr222hn@student.lnu.se>
2013-12-14 13:43:08 -0500

    Klar med labb : game on ej redovisad

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,12 +1,13 @@
+
 "use strict"
 var cols = 4;
 var rows = 4;
 
 var Memory= {
-    
-    
-    
-// Array som används som egenskap i objektet som refererar till den array med de slumpade resultaten, här de kommer lagras. 
+
+
+
+// Array som används som egenskap i objektet som refererar till den array med de slumpade resultaten, här de kommer lagras.
 randomMemory: [],
 
 //Array som lagrar alla a-taggat.
@@ -17,107 +18,102 @@ countPair: 0,
 
 // Antal försök
 countTries: 0,
-   
+
 init:function(){
-   
-    
+
+
 // Här anropas "getPictureArray" i filen "random.js" som slumpar bilderna och sparar resultatet i randomMemory arrayen ovan.
 Memory.randomMemory = RandomGenerator.getPictureArray(cols, rows);
 
 //Anropar "createTable" för att kunna visa tabellen.
-Memory.createTable();},
+Memory.createTable();
+
+},
 
 //Funktion som ritar upp själva tabellen.
 createTable:function(){
-     var mem= 0;
-     
+
     // För att skapa en tabell i funktionen createTable som sedan anropas av "Memory.CreateTable" som visar tabellen i fönstret.
     var table = document.createElement("table");
-    
+
     // Sätter in i "memoryBoard" i html filen.
     document.getElementById("memoryBoard").appendChild(table);
     
+    var mem= 0;
+
     // Genererar upp tabellen som bilderna kommer ligga i. Loopar med hjälp av en for sats, de antal rader som tabellen ska innehålla.
-    for (var row = 0; row< rows; row++){
-      
+    for (var row = 0; row < rows; row++){
+
       //Lägger in alla loopade "tr" taggar i variabeln table.
         var tr = document.createElement("tr");
         table.appendChild(tr);
-        
+
         // Genererar upp antal kolumner i tabellen.
-        for (var col=0; col<cols; col++){
-            
+        for (var col=0; col < cols; col++){
+
         // Lägger in alla loopade "td" taggar i de loopade "tr" taggar som redan skapats.
         var td = document.createElement("td");
         tr.appendChild(td);
-        
+
         // Skapar "img" taggen och tilldelar den, den första bilden i mappen pics (0.png).
         var img = document.createElement("img");
         img.setAttribute("src", "pics/0.png");
-        
+        img.id = mem;
+
         //Skapar a taggen, sätter sedan in "img" taggen i "a" taggen, därefter sätter in "a" taggen i "td" taggen.
         var a = document.createElement("a");
         a.setAttribute("href","#");
         a.appendChild(img);
         td.appendChild(a);
+
         
-        //Anropar funktionen turn.
-        Memory.turn(mem, a);
+            Memory.turn(mem, a);
         
         // Räknar upp mem.
-        mem++;
+            mem++;
+        }
         
         }
         
-    }
-    
-},
-
-turn: function(q, b){
-    
-    console.log(q, b);
-    
+        },
 
     
-    b.onclick= function(){
-      
-      
-      // Ska vara startbilden "0.png" och ej ngt annat, annars retunera "False", så alltid frågetecknet visas innan mus-klick.
+
+
+
+turn: function(q, r){
+
+    r.onclick = function(){
         if(this.getElementsByTagName("img")[0].getAttribute("src") !== "pics/0.png")
         {
             return false;
         }
-    };
-    
-    console.log(b);
-    
-    // Sätter in "a" taggen i arrayen "atags"
-    Memory.atags.push(1);
-   // Om arrayens längd bara uppnår 1 eller 2 element , visas bilden upp, alltså vid varje klick (ska bara kunna klicka på startbilden för vändning.
-   if (Memory.atags.length === 1 || Memory.atags.length === 2) {
-        console.log(Memory.randomMemory[q]);
         
+        //Lägger in a taggen i arrayen
+        Memory.atags.push(r);
+    
+// Om arrayens längd bara uppnår 1 eller 2 element , visas bilden upp, alltså vid varje klick (ska bara kunna klicka på startbilden för vändning.
+   if (Memory.atags.length === 1 || Memory.atags.length === 2) 
+   {
         this.getElementsByTagName("img")[0].setAttribute("src", "pics/" + Memory.randomMemory[q] + ".png");
     }
-    
-    //Skulle bilderna ej stämma, anropas funktionen "seeMems" för att vända tillbaka efter 1 sec.
-    if (Memory.atags.length == 2)
-    { setTimeout(function(){ Memory.seeMems(Memory.atags);
-    }, 1000);}
 
- 
+    if (Memory.atags.length === 2)
+    {
+        setTimeout(function(){
+    Memory.seeMems(Memory.atags);
+        }, 1000);
+    }
+
+};
 },
 
 
-
-
-
-
-// Testar om rutorna är likadana så kommer den visas annars vänds dem tillbaka. 
+// Testar om rutorna är likadana så kommer den visas annars vänds dem tillbaka.
 seeMems: function(memo){
 if (memo[0].getElementsByTagName("img")[0].getAttribute("src") === memo[1].getElementsByTagName("img")[0].getAttribute("src"))
 {
-    
+
 // Räkna upp antal rätta par
 Memory.countPair++;
 
@@ -138,18 +134,11 @@ memo[1].getElementsByTagName("img")[0].setAttribute("src", "pics/0.png");
 
 Memory.atags = [];
 
-// och räknas upp som ett försök
+// och räknas upp som ett nytt försök
 Memory.countTries++;
 }
-
-
 }
 };
-
-
-
-
-
 
 
  window.onload = Memory.init;
