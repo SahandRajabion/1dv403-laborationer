fa9e48afb6e804060da4378600a7d93dc65cb4bd
OpenShift guest <sr222hn@student.lnu.se>
2013-12-18 20:50:56 -0500

    validering av efternamn + postnummer + ändringar av span i html + tillägg i css filen för error meddelanden och input

--- 4-forms/formular.js
+++ 4-forms/formular.js
@@ -15,13 +15,13 @@ var Validator = {
              // Hämtar ut taggar från html filen och tilldelar dem variablar.
              
              
-             var firstname = document.getElementById("Förnamn");
+             var firstname = document.getElementById("firstN");
              var lastname = document.getElementById("Efternamn");
              var email = document.getElementById("Epost");
              var postnr = document.getElementById("Postnummer");
              var fieldset = document.getElementById("formular");
              var form = document.getElementById("form");
-             var send = document.getElementById("skicka");
+             var button = document.getElementById("skicka");
              
              
             // Validering av Förnamn , när användare går över till nästa fält.
@@ -29,9 +29,9 @@ var Validator = {
             firstname.onblur = function(){
                 
                 // Kontrollerar Whitespace, mellanrum mellan text i första fältet.
-                if(!form.element["firstname"]. value.match(/^[a-zåäö]+$/i))
+                if(!form.elements["firstN"].value.match(/^[a-zåäö]+$/i))
                 { 
-                    form.elements["firstname"].className ="error";
+                    form.elements["firstN"].className = "error";
                     
                     //Skickar ej om valideringen ej går igenom.
                     Validator.first = false;
@@ -40,7 +40,7 @@ var Validator = {
                      {
                          
                          var div = document.createElement("div");
-                         div.classname ="failedMessage";
+                         div.className ="failedMessage";
                          document.getElementById("first").appendChild(div);
                          
                          var text = document.createTextNode("Skriv in ditt namn!");
@@ -53,27 +53,88 @@ var Validator = {
              
              else
              {
-                 form.elements["firstname"].classname = "valid";
+                 form.elements["firstN"].className = "valid";
              Validator.first = true;
              var elements = document.getElementById("first");
-             while (elements.firstChild); { elements.removeChild(elements.firstChild);
+             while (elements.firstChild) { elements.removeChild(elements.firstChild);
              
              
              }
              
             }
   
-  
+            };
+            
+         //Validering för efternamn , andra fältet. Valideras när fält är ur "focus".
+         lastname.onblur = function(){
+             if(!form.elements["Efternamn"].value.match(/^[a-zåäö]+$/i)){
+                 
+                 form.elements["Efternamn"].className = "error";
+                    
+                    //Skickar ej om valideringen ej går igenom.
+                    Validator.last = false;
+                    
+                    if(!document.getElementById("last").hasChildNodes())
+                     {
+                         
+                         var div = document.createElement("div");
+                         div.className ="failedMessage";
+                         document.getElementById("last").appendChild(div);
+                         
+                         var text = document.createTextNode("Skriv in ditt Efternamn!");
+                         div.appendChild(text);
+             }
+             
+             }
+             
+             
+             else
+             {
+                 form.elements["Efternamn"].className = "valid";
+             Validator.last = true;
+             var elements = document.getElementById("last");
+             while (elements.firstChild) {
+                 elements.removeChild(elements.firstChild);
+             }
+             }
+             };
+             
+             
+             postnr.onblur = function(){
+                 if (form.elements["Postnummer"].value.match(/^(SE)?[\ ]?[0-9]{3}(-|\ )?[0-9]{2}$/))
+                 { 
+                 form.elements["Postnummer"].value = form.elements["Postnummer"].value.replace(/(SE|\ |-)/g, '');
+                 form.elements["Postnummer"].classname="valid";
+                 
+                 Validator.nummer= true;
+                 var elements = document.getElementById("nummer");
+                 while(elements.firstChild){
+                     elements.removieChild(elements.firstChild);
+                 }
+                 }
+                 
+                 else {
+                  form.elements["Postnummer"].classname="error";
+                 Validator.nummer=false;
+                 
+                 
+                 
+                 if(!document.getElementById("nummer").hasChildNodes()){
+                     var div =document.CreateElement("div");
+                     div.classname= "failedMessage";
+                     document.getElementById("nummer").appendChild(div);
+                     
+                     var text = document.createTextNode("Felaktigt postnummer, försök igen!");
+                     div.appendchild(text);
+                     
+                 }
+         
+                 }
+
+                 };
+             
+             
     
-    
-    };
+   
          
         window.onload = Validator.init;
- 
-  
-  
-  
-  
-  
-  
-  
